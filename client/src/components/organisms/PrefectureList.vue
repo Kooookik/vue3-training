5c4f543bd3764d8bdb6774504e8e0ee0436c2a55
55005 <saginuma-koki@ksc.co.jp>
2023-09-19 16:33:43 +0900

    2023/9/19

--- client/src/components/organisms/PrefectureList.vue
+++ client/src/components/organisms/PrefectureList.vue
@@ -1,9 +1,32 @@
 <script lang="ts" setup>
-import { onMounted, ref } from "vue"
-import axiosInstance from "@/utils/axiosSettings"
 import { PrefectureDisplay } from "@/types/prefecture"
+import axiosInstance from "@/utils/axiosSettings"
+import { onMounted, ref } from "vue"
 // TODO: prefectureで千葉県を表示してみましょう
-const prefecture = ref<PrefectureDisplay>({})
+// eslint-disable-next-line camelcase
+const prefecture_12 = ref<PrefectureDisplay>({
+  prefCode: 12,
+  prefName: "千葉県",
+  isCheck: true
+})
+
+// eslint-disable-next-line camelcase
+const prefecture_13 = ref<PrefectureDisplay>({
+  prefCode: 13,
+  prefName: "東京都",
+  isCheck: true
+})
+
+// eslint-disable-next-line camelcase
+const prefecture_14 = ref<PrefectureDisplay>({
+  prefCode: 14,
+  prefName: "神奈川県",
+  isCheck: true
+})
+
+// function check(bool: boolean) {
+//   prefecture.value.isCheck = bool
+// }
 
 onMounted(() => {
   // TODO: 全県取得のAPIへリクエストを送ってみましょう!
@@ -15,6 +38,19 @@ onMounted(() => {
     <h3>都道府県</h3>
     <div class="prefecture-flex">
       <!-- TODO: 県を表示してみましょう -->
+      {{ prefecture_12.prefName }}
+      <input v-model="prefecture_12.isCheck" type="checkbox" />
+      <!-- @change="check(!prefecture.isCheck)" v-model使えばこれいらない-->
+      <span v-if="prefecture_12.isCheck">true</span>
+      <span v-else>false</span>
+    </div>
+    <div class="prefecture-flex">
+      {{ prefecture_13.prefName }}
+      <input v-model="prefecture_13.isCheck" type="checkbox" />
+    </div>
+    <div class="prefecture-flex">
+      {{ prefecture_14.prefName }}
+      <input v-model="prefecture_14.isCheck" type="checkbox" />
     </div>
   </div>
 </template>
